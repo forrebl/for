4b83c32ee6635a8e3e7e9935e17c4415ab3fcce2
forrebl <forrebl@gmail.com>
2026-08-12 05:45:02 +0300

    Add games section routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
@@ -6,6 +6,8 @@ import ProjectDetail from './pages/ProjectDetail';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Game from './pages/Game';
+import Games from './pages/Games';
+import CatchGame from './pages/CatchGame';
 import NotFound from './pages/NotFound';
 
 export const router = createBrowserRouter([
@@ -17,7 +19,10 @@ export const router = createBrowserRouter([
       { path: 'projects', element: <Projects /> },
       { path: 'project/:id', element: <ProjectDetail /> },
       { path: 'about', element: <About /> },
-      { path: 'game', element: <Game /> },
+      { path: 'games', element: <Games /> },
+      { path: 'games/cmyk', element: <Game /> },
+      { path: 'games/catch', element: <CatchGame /> },
+      { path: 'game', element: <Navigate to="/games/cmyk" replace /> },
       { path: 'contact', element: <Contact /> },
       { path: '*', element: <NotFound /> },
     ],
